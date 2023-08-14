06e3202457a337f2cd6276c2461b610f294989f9
jaime <silvacastillojaime@gmail.com>
2023-08-13 23:28:50 -0400

    localStorage y jSon

--- js/funciones.js
+++ js/funciones.js
@@ -1,211 +1,259 @@
-
-
-let tipoPago = ['(1) ->Efectivo','(2) -> Debito', '(3) -> Credito','(4) -> Cheque', '(0) -> Salir'];
+let tipoPago = ['(1) ->Efectivo', '(2) -> Debito', '(3) -> Credito', '(4) -> Cheque', '(0) -> Salir'];
+let montosGuardados = JSON.parse(localStorage.getItem('detalles')) || [];
 
 const registro = [];
 let tablaBody = document.getElementById('tablabody');
-//let mTotal = document.getElementById('total');
 let mTotal = document.getElementById('total').innerText;
 
-let total=0;
-let efectivo=0;
-let debito=0;
-let credito=0;
-let cheque=0;
+let total = 0;
+let efectivo = 0;
+let debito = 0;
+let credito = 0;
+let cheque = 0;
+let nombre=''
 
+function actualizarTabla() {
+    
+    montosGuardados = JSON.parse(localStorage.getItem('detalles')) || [];
+    montosGuardados.forEach((detalle, index) => {
 
-function sumaTotal(monto){
-  if(monto>0){
-    total+=monto;
-    console.log(total);
-  }
+        tablaBody.innerHTML += `
+        <tr data-id='${index + 1}'>
+          <td>${index + 1}</td>
+          <td>${detalle.detalle}</td>
+          <td>${detalle.monto}</td>
+          <td><button type="button" class="btn btn-outline-danger btnELim">❌</button></td>
+        </tr>
+        `;
+        total += detalle.monto;
+          
+    });
+    let paso = devuelveMiles(String(total));
+    document.getElementById('total').innerText = mTotal + paso
+
+    nombre = localStorage.getItem('nomCajero')
+    if(nombre != null){
+        console.log(nombre);
+        const name = document.getElementById('nombre').innerHTML;
+        document.getElementById('nombre').innerHTML = `
+        <h4>${name} ${nombre.toUpperCase()}</h4>
+        ` ;
+    }
+        
+    
+}
+
+actualizarTabla();
+
+function sumaTotal(monto) {
+    if (monto > 0) {
+        total += monto;
+        console.log(total);
+    }
 }
 
 function focusInit() {
-    
+
     document.getElementById("flexRadioEfectivo").focus();
 }
-  
-function devuelveMiles(monto){
+
+function devuelveMiles(monto) {
     let nuevoMonto = [];
 
     for (let i = monto.length - 1; i >= 0; i -= 3) {
-      let ini = Math.max(0, i - 2);
-      let cad = monto.substring(ini, i + 1);
-      nuevoMonto.unshift(cad);
+        let ini = Math.max(0, i - 2);
+        let cad = monto.substring(ini, i + 1);
+        nuevoMonto.unshift(cad);
     }
-  
-    return(nuevoMonto.join('.'));
+    return (nuevoMonto.join('.'));
 }
 
-function igresaCajero(){
-   
+
+function igresaCajero() {
     var modalNombre = new bootstrap.Modal(document.getElementById("modalNombre"));
 
-    if(registro ==""){
+    if (registro == "") {
 
-        // let nombre = prompt('Igrese nombre: ');
         const nombre = document.getElementById('floatingTextareaNombre').value;
         const name = document.getElementById('nombre').innerHTML;
         console.log(nombre);
-        document.getElementById('nombre').innerHTML=`
+        document.getElementById('nombre').innerHTML = `
             <h4>${name} ${nombre.toUpperCase()}</h4>
         ` ;
-    }
-    modalNombre.dispose();  
-}
 
+        localStorage.setItem('nomCajero', nombre);
+    }
+    
+    modalNombre.dispose();
+}
 
 // obtener valor del radio button
 const radioOp = document.querySelectorAll('input[name="flexRadioDefault"]');
 let valorRadio = "flexRadioEfectivo";
-let montoSumando=0
+let montoSumando = 0
 radioOp.forEach(radio => {
-  radio.addEventListener('change', function(event) {
-    if (event.target.checked) {
-      
-        valorRadio = radio.id;
-        
-        console.log('Radio seleccionado:', valorRadio);
-    }
-  });
+    radio.addEventListener('change', function (event) {
+        if (event.target.checked) {
+
+            valorRadio = radio.id;
+        }
+    });
 });
+
+const botonAgregaMonto = document.getElementById('btnAgregaMonto');
+const texto = document.getElementById('textSuma');
+botonAgregaMonto.onclick = () => {
+    const texto = document.getElementById('textSuma');
+    texto.focus();
+}
 
 const botonSuma = document.getElementById('buttonSuma');
 botonSuma.onclick = () => {
     console.log('aki suma');
-    
+
     ingresa();
-  
-    
-    document.getElementById('textSuma').value='';
+
+    document.getElementById('textSuma').value = '';
     document.getElementById('textSuma').focus();
 }
 
-document.getElementById('textSuma').addEventListener('keydown', function(event) {
+document.getElementById('textSuma').addEventListener('keydown', function (event) {
     if (event.key === 'Enter') {
-        event.preventDefault(); 
-        document.getElementById('textSuma').value = ''; 
-
-        console.log('le diste enter');
-
+        
+        event.preventDefault();
         ingresa();
+        document.getElementById('textSuma').value = '';
     }
 });
 
-function SetSubtotal(monto){
+
+function SetSubtotal(monto) {
     let sub = document.getElementById('badge').innerText;
-    console.log( "a la suma " + monto + " - "  +sub)
-    document.getElementById('subTotal').innerText = monto + parseFloat(sub);
+    let suma = parseFloat(sub) + monto;
+    document.getElementById('badge').innerText = suma;
 }
 
-function ingresa(){
+function ingresa() {
 
-    console.log('funcion ingresa')
-    
+
     let op = valorRadio;
-    montoSumando=parseFloat(document.getElementById('textSuma').value);
-    console.log(montoSumando);
-    if(!isNaN(montoSumando)){
-        
-        while(op != ''){
-            switch(op){
+    montoSumando = parseFloat(document.getElementById('textSuma').value);
+
+    if (!isNaN(montoSumando)) {
+
+        while (op != '') {
+            switch (op) {
                 case 'flexRadioEfectivo':
-                    // let efect = parseFloat(prompt('Ingrese el efectivo a sumar'));
                     let efect = parseFloat(montoSumando);
                     sumaTotal(efect);
-                    efectivo+=efect;
-                    agregaMontos(['Efectivo',efect])
-                    
-                    SetSubtotal(montoSumando)  
+                    efectivo += efect;
+                    agregaMontos(['Efectivo', efect])
 
-                break;
+                    SetSubtotal(montoSumando)
+
+                    break;
                 case 'flexRadioDebito':
                     let debi = parseFloat(montoSumando)
                     sumaTotal(debi);
-                    debito+=debi;
-                    agregaMontos(['Débito',debi])
-                break;
+                    debito += debi;
+                    agregaMontos(['Débito', debi])
+                    SetSubtotal(montoSumando)
+                    break;
                 case 'flexRadioCredito':
                     let credi = parseFloat(montoSumando);
                     sumaTotal(credi);
-                    credito+=credi;
-                    agregaMontos(['Crédito',credi]);
-                break;
+                    credito += credi;
+                    agregaMontos(['Crédito', credi]);
+                    SetSubtotal(montoSumando)
+                    break;
                 case 'flexRadioCheque':
                     let chec = parseFloat(montoSumando);
                     sumaTotal(chec);
-                    cheque+=chec;
-                    agregaMontos(['Cheque',chec]);
-                break;
+                    cheque += chec;
+                    agregaMontos(['Cheque', chec]);
+                    SetSubtotal(montoSumando)
+                    break;
                 default:
                     alert('Opcion no valida..🤷🏻‍♂️');
-                break
-                
+                    break
             }
-            
-                op= '';
-    
+            op = '';
         }
-        
-        let paso= devuelveMiles(String(total));
-        document.getElementById('total').innerText = mTotal + paso;
-        
 
+        let paso = devuelveMiles(String(total));
+        document.getElementById('total').innerText = mTotal + paso;
     }
 }
 
 function agregaMontos(detalle) {
     registro.push(detalle);
     console.table(registro);
-    
-        tablaBody.innerHTML += `
-        <tr>
-            <td>${registro.indexOf(detalle)}</td>
+    // <tr data-id='${index+1}'></tr>
+    tablaBody.innerHTML += `
+        <tr id="5555">
+            <td>${registro.indexOf(detalle) + 1}</td>
             <td>${detalle[0]}</td>
             <td>${detalle[1]}</td>
+            <td><button type="button" class="btn btn-outline-danger btnELim">❌</button></td>
         </tr>
     `;
-
-}
-function elimina(){
-
-    if(registro != ""){
-        idMod = parseInt(prompt('ingresa ID del registro que desas eliminar : \n'))
-        const num = document.getElementById('tablabody')
-       //for(const reg in registro){
-            //console.log(num)
-            const resultado = registro.map((valor, indice) => {
-                // return `Índice ${indice}: ${valor}`;
-                //const este = `${valor}`;
-                const este2 = `${indice}`
-                
-                
-                if(este2 == idMod){
-                    console.log(este2);
-                    registro.splice(este2)
-                   // location.reload();
-                   document.getElementById('tablabody').innerHTML = '';
-                    console.log()
-                    refresca(este2)
-                }
-            });
-              
-    }else{
-        alert('Resgistro vacio, imposible modificar..! 🤷🏻‍♂️')
-    }
+    
+    guardarEnLocalStorage(detalle[0], detalle[1]);
 }
 
-function refresca(){
-    for(regis in registro){
-
-        console.log(regis)
-        agregaMontos(regis)
-    }
-
-    agregaMontos(registro)
+function guardarEnLocalStorage(detalle, monto) {
+    const detalles = { detalle, monto };
+    montosGuardados.push(detalles);
+    localStorage.setItem('detalles', JSON.stringify(montosGuardados));
 }
 
-function refrescaPagina(){
-    location.reload();
+// limpia el strorage y recarga la pagina
+const btnLimpiar = document.getElementById('limpiar');
+btnLimpiar.onclick = () => {
+    localStorage.clear();
+    location.reload(true);
 }
+
+const btnElimina = document.querySelectorAll('.btnELim');
+btnElimina.forEach(boton => {
+    boton.addEventListener('click', (e) => {
+
+        console.log('aki', btnElimina.values)
+
+        const fila = e.target.closest('tr');
+        const id = fila.dataset.id;
+
+        const indiceBuscado = id - 1; 
+
+        const objetosGuardados = JSON.parse(localStorage.getItem('detalles')) || [];
+        console.log(indiceBuscado);
+        if (indiceBuscado >= 0 && indiceBuscado < objetosGuardados.length) {
+            objetosGuardados.splice(indiceBuscado, 1); // Eliminar el registro del arreglo
+            localStorage.setItem('detalles', JSON.stringify(objetosGuardados));
+            console.log('Registro eliminado.');
+        } else {
+            console.log('Índice fuera de rango o no encontrado.');
+        }
+        actualizarTabla();
+        location.reload(true);
+    });
+});
+
+const btnAcepta = document.getElementById('acepta');
+btnAcepta.onclick  = () => {
+    location.reload(true);
+}
+
+const miModal = new bootstrap.Modal(document.getElementById('exampleModalToggle2'));
+miModal._element.addEventListener('shown.bs.modal', function (event) {
+    console.log('Modal abierto');
+    let textoFocus = document.getElementById('textSuma');
+    textoFocus.focus();
+});
+
+const miModalNuevoMonto = new bootstrap.Modal(document.getElementById('modalNombre'));
+miModalNuevoMonto._element.addEventListener('shown.bs.modal', function (event) {
+    console.log('Modal abierto');
+    let textoFocus = document.getElementById('floatingTextareaNombre');
+    textoFocus.focus();
+});
